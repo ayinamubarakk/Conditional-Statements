738e0e93e658c6a74f789f5fa2065c95304fed6b
Ayina Mubarakk <ayina.mubarak2301051@gmail.com>
2025-11-15 17:09:13 +0500

    Add files via upload

---
+++ Conditional Statements.py
@@ -0,0 +1,34 @@
+# age = 21
+
+# if (age >= 18):
+#     print("can vote")
+#     print("can drive")
+
+light = "purple "
+
+if light == "red":
+    print("Stop")
+
+elif light == "green":
+    print("Go")
+
+elif light == "yellow":
+    print("Look")
+
+else:
+    print("Light is broken")
+
+print("End of code")
+
+# marks = 20
+# if marks >= 10:
+#     print("Not Eligible")
+
+# elif marks <= 10:
+#     print("Eligible")
+
+# else:
+#     print("Not a student")
+
+
+    
